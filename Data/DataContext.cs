071c1f4f52ea6c500558752b108158de54409f70
Chkhuto's _Hub <ana.chkhutiashvili.1@btu.edu.ge>
2024-12-13 14:44:28 +0400

    upload

---
+++ Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using BankAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(a => a.Balance)
+                .HasPrecision(18, 2); 
+            modelBuilder.Entity<Transaction>()
+                .Property(a => a.Amount)
+                .HasPrecision(18, 2);
+        }
+
+    }
+}
